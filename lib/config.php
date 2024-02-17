3041fbd0bfc2fc2d2dfe7e1b5379a2bd4aa3e4cc
YAYA Abdou-baki <baki.abdou@yahoo.com>
2024-02-17 13:19:33 +0100

    modification gitignore

---
+++ lib/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("_DOMAIN_", ".garage_automobile_vp.local");
+define("_DB_SERVER_", "localhost");
+define("_DB_NAME_", "garage_automobile_vp");
+define("_DB_USER_", "vincent_parrot"); // "vincent_parrot"
+define("_DB_PASSWORD_", "3f7zhhRn4NH69R23"); // "3f7zhhRn4NH69R23"
+
+define("_ASSETS_IMAGES_FOLDER_", "/assets/images/car_default.jpg");
+define("_CARS_IMAGES_FOLDER_", "/uploads/articles/");
+define("_PRESTATIONS_IMAGES_FOLDER_", "/uploads/prestations/");
+define("_HOME_ARTICLES_LIMIT_", 4);
+define("_ADMIN_ITEM_PER_PAGE_", 10);
