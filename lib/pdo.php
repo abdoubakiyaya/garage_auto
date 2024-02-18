c85836964a9139ae5e3fa2763527553b652540f8
YAYA Abdou-baki <baki.abdou@yahoo.com>
2024-02-18 10:23:03 +0100

    configuration de la bd jawsdb pour deploiement

--- lib/pdo.php
+++ lib/pdo.php
@@ -2,8 +2,25 @@
 require_once __DIR__ . "/config.php";
 
 
+//
+if (getenv('JAWSDB_URL') !== false) {
+  $dbparts = parse_url(getenv('JAWSDB_URL'));
+
+  $hostname = $dbparts['host'];
+  $username = $dbparts['user'];
+  $password = $dbparts['pass'];
+  $database = ltrim($dbparts['path'], '/');
+} else {
+  // connexion locale
+  $hostname = 'localhost';
+  $username = 'vincent_parrot';
+  $password = '3f7zhhRn4NH69R23';
+  $database = 'garage_automobile_vp';
+}
+
 try {
-  $pdo = new PDO("mysql:dbname=" . _DB_NAME_ . ";host=" . _DB_SERVER_ . ";charset=utf8mb4", _DB_USER_, _DB_PASSWORD_);
-} catch (Exception $e) {
+  $pdo = new PDO("mysql:host={$hostname};dbname={$database};charset=utf8mb4", $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
   die('Erreur : ' . $e->getMessage());
 }
