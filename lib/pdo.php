9b3926f806f9af8a647a63a913c681df24d5dab2
YAYA Abdou-baki <baki.abdou@yahoo.com>
2024-02-19 13:54:39 +0100

    pdo

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,22 +1,50 @@
 <?php
 require_once __DIR__ . "/config.php";
 
+require_once __DIR__ . "/config.php";
 
-$url = getenv('JAWSDB_URL');
 
-if ($url) {
-  $dbparts = parse_url($url);
+//
+if (getenv('JAWSDB_URL') !== false) {
+  $dbparts = parse_url(getenv('JAWSDB_URL'));
+
   $hostname = $dbparts['host'];
   $username = $dbparts['user'];
   $password = $dbparts['pass'];
   $database = ltrim($dbparts['path'], '/');
 
-  try {
-    $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    echo "Connexion à la base de données JawsDB réussie.";
-  } catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-  }
+  $sqlContent = file_get_contents('backup.sql');
+  $pdo->exec($sqlContent);
 } else {
-  echo "La variable d'environnement JAWSDB_URL n'est pas définie.";
+  // connexion locale
+  $hostname = 'localhost';
+  $username = 'root';
+  $password = '';
+  $database = 'garage_automobile_vp';
 }
+
+try {
+  $pdo = new PDO("mysql:host={$hostname};dbname={$database};charset=utf8mb4", $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die('Erreur : ' . $e->getMessage());
+}
+
+// $url = getenv('JAWSDB_URL');
+
+// if ($url) {
+//   $dbparts = parse_url($url);
+//   $hostname = $dbparts['host'];
+//   $username = $dbparts['user'];
+//   $password = $dbparts['pass'];
+//   $database = ltrim($dbparts['path'], '/');
+
+//   try {
+//     $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+//     echo "Connexion à la base de données JawsDB réussie.";
+//   } catch (PDOException $e) {
+//     echo "Erreur de connexion : " . $e->getMessage();
+//   }
+// } else {
+//   echo "La variable d'environnement JAWSDB_URL n'est pas définie.";
+// }
